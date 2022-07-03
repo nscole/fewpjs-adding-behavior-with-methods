5ee268cd90bb3823d280ed56a944c2a762eeb9a6
Naomi Cole <naomisarahcole@gmail.com>
2022-07-03 13:02:46 +1000

    passing 4 tests

--- index.js
+++ index.js
@@ -2,10 +2,20 @@
 
 class Cat {
     constructor(name, sex){
-      this.name = name
-      this.sex = sex
+      this.name = name;
+      this.sex = sex;
     }
     speak(){
       return `${this.name} says meow!`
     }
 }
+
+class Dog {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says woof!`
+    }
+};
